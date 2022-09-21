1436cb365e5abf5c0f7cfabbcf9401f82c9c0bc2
Dian Hafidh Zulfikar <dyan.hz180@gmail.com>
2022-09-21 13:53:27 +0700

    tambah file baru

---
+++ ArraySample.java
@@ -0,0 +1,11 @@
+package sda2022_dianhafidhzulfikar;
+
+public class ArraySample {
+
+  public static void main(String[] args) {
+    int ages[] = new int[5];
+    for (int index = 0; index < ages.length; index++) {
+      System.out.println(ages[index]);
+    }
+  }
+}
---
+++ belajarArray.java
@@ -0,0 +1,5 @@
+public class belajarArray {
+  public static void main(String[] args) {
+    System.out.println("Hallo");
+  }
+}
